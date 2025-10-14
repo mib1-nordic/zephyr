385cfcaf262cc00577a074e17a343aee3108e393
Michał Bainczyk <michal.bainczyk@nordicsemi.no>
2025-10-14 15:08:04 +0200

    samples: hello_world: Modify the sample according to the instructions
    Modify the sample according to the instructions in an attempt to
    reproduce the issue.
    
    Signed-off-by: Michał Bainczyk <michal.bainczyk@nordicsemi.no>

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -4,11 +4,17 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <zephyr/kernel.h>
 #include <stdio.h>
 
 int main(void)
 {
 	printf("Hello World! %s\n", CONFIG_BOARD_TARGET);
-
+#if 1
+	while (1) {
+		k_sleep(K_USEC(100));
+		k_busy_wait(100);
+	}
+#endif
 	return 0;
 }
